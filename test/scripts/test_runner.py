7a7f1d139b991a09c75d0093775f5e57796bcd22
Bailey Forrest <baileycforrest@gmail.com>
2015-04-18 14:59:07 -0400

    Multithreaded test runner

--- test/scripts/test_runner.py
+++ test/scripts/test_runner.py
@@ -4,10 +4,11 @@ import os
 import sys
 import subprocess
 import time
+import multiprocessing
 
 RUNTIME = "./test/util/runtime.c"
 HEADER_STR = "//test"
-TEMP_NAME = "temp"
+TEMP_DIR = "/tmp/ccc/"
 LLVM_SUFFIX = "ll"
 TIMEOUT = 5
 CC = "cc"
@@ -15,58 +16,89 @@ CLANG = "clang"
 
 DEV_NULL = open(os.devnull, 'w')
 
+NUM_PROCS = 4
+
 failure = False
+verbose = False
+llvm = False
+compiler_opts = None
 
 def usage():
-    print("usage: %s [-llvm] [-v] [compiler and options] [tests] \n"
+    print("usage: %s [-llvm] [-v] [-jN] [compiler and options] [tests] \n"
           % sys.argv[0])
     print("test must be a c source file with a correct first line header:")
     print("%s [return n | error | exception | noreturn ]" % HEADER_STR)
 
     print("\noptional arguments:");
+    print("-j Jobs to run in parallel")
     print("-v Verbose - Prints passed tests and compiler's stdout/stderr")
     print("-llvm Compiles to llvm ir")
     sys.exit(-1)
 
-def fail(src_name, msg):
+def fail(src_path, msg):
     global failure
     failure = True
-    print("FAIL: " + src_name + " " + msg)
+    print("FAIL: " + src_path + " " + msg)
 
-def success(src_name, verbose):
+def success(src_path, verbose):
     if verbose:
-        print("PASS: " + src_name)
+        print("PASS: " + src_path)
 
 def main():
+    global verbose
+    global llvm
+    global compiler_opts
+
     if len(sys.argv) < 3:
         usage()
 
-    verbose = False
-    llvm = False
-
     args = sys.argv[1:]
+    jobs = 1
 
     while args[0][0] == "-":
         if args[0] == "-llvm":
             llvm = True
         elif args[0] == "-v":
             verbose = True
+        elif args[0][:2] == "-j":
+            jobs = int(args[0][2:])
         else:
             usage()
         args = args[1:]
 
     compiler_opts = args[0].split()
-    for src_name in args[1:]:
-        src = open(src_name)
+    src_files = args[1:]
+
+    if not os.path.exists(TEMP_DIR):
+        os.makedirs(TEMP_DIR)
+
+
+    if jobs == 1:
+        for src_path in src_files:
+            process_file(src_path)
+    else:
+        pool = multiprocessing.Pool(processes=jobs)
+        pool.map(process_file, src_files)
+
+    if failure:
+        sys.exit(1)
+
+
+def process_file(src_path):
+    temp_files = []
+
+    try:
+        src = open(src_path)
         header = src.readline()
         src.close()
+        src_name = os.path.splitext(os.path.basename(src_path))[0]
 
         header = header[:len(header) - 1] # Remove trailing newline
         header = header.split(" ")
 
         if len(header) < 2 or header[0] != HEADER_STR:
-            fail(src_name, "Invalid header")
-            continue
+            fail(src_path, "Invalid header")
+            return
 
         is_return = False
         return_val = 0
@@ -76,8 +108,8 @@ def main():
 
         if header[1] == "return":
             if len(header) < 3:
-                fail(src_name, "Invalid header")
-                continue
+                fail(src_path, "Invalid header")
+                return
             is_return = True
             return_val = int(header[2])
         elif header[1] == "error":
@@ -87,73 +119,90 @@ def main():
         elif header[1] == "noreturn":
             is_noreturn = True
         else:
-            fail(src_name, "Invalid header")
-            continue
+            fail(src_path, "Invalid header")
+            return
 
         timeout_remain = TIMEOUT
 
-        if llvm:
-            outname = TEMP_NAME + "." + LLVM_SUFFIX
-        else:
-            outname = TEMP_NAME
-
         start = time.clock()
-        try:
-            if verbose and not is_error:
-                retval = subprocess.call(
-                    compiler_opts + ["-o", "./" + outname, src_name, RUNTIME],
-                    timeout=timeout_remain)
-            else:
-                retval = subprocess.call(
-                    compiler_opts + ["-o", "./" + outname, src_name, RUNTIME],
-                    timeout=timeout_remain, stdout=DEV_NULL,
-                    stderr=subprocess.STDOUT)
-        except subprocess.TimeoutExpired:
-            fail(src_name, "Compile timed out")
-            continue
+        if llvm:
+            outname = TEMP_DIR + src_name + "." + LLVM_SUFFIX
+            temp_files.append(outname)
+            try:
+                if verbose and not is_error:
+                    retval = subprocess.call(
+                        compiler_opts + ["-o", outname, src_path],
+                        timeout=timeout_remain)
+                else:
+                    retval = subprocess.call(
+                        compiler_opts + ["-o", outname, src_path],
+                        timeout=timeout_remain, stdout=DEV_NULL,
+                        stderr=subprocess.STDOUT)
+            except subprocess.TimeoutExpired:
+                fail(src_path, "Compile timed out")
+                return
+        else:
+            outname = TEMP_DIR + src_name
+            temp_files.append(outname)
+            try:
+                if verbose and not is_error:
+                    retval = subprocess.call(
+                        compiler_opts + ["-o", outname, src_path, RUNTIME],
+                        timeout=timeout_remain)
+                else:
+                    retval = subprocess.call(
+                        compiler_opts + ["-o", outname, src_path, RUNTIME],
+                        timeout=timeout_remain, stdout=DEV_NULL,
+                        stderr=subprocess.STDOUT)
+            except subprocess.TimeoutExpired:
+                fail(src_path, "Compile timed out")
+                return
 
         end = time.clock()
-        timeout_remain -= end - start
 
-        if llvm:
-            asm_name = TEMP_NAME + "." + "S"
-            retval = subprocess.call([CLANG, outname, RUNTIME, "-o", asm_name])
-            if retval != 0:
-                fail(src_name, "failed to create valid llvm ir")
-                continue
+
+        timeout_remain -= end - start
 
         if is_error:
             if retval == 0:
-                fail(src_name, "Compilation unexpectedly suceeded")
+                fail(src_path, "Compilation unexpectedly suceeded")
             else:
-                success(src_name, verbose)
-            continue
+                success(src_path, verbose)
+            return
 
         if retval != 0:
-            fail(src_name, "failed to compile")
-            continue
+            fail(src_path, "failed to compile")
+            return
+
+        if llvm:
+            exec_name = TEMP_DIR + src_name
+            temp_files.append(exec_name)
+            retval = subprocess.call([CLANG, outname, RUNTIME, "-o", exec_name])
+            if retval != 0:
+                fail(src_path, "failed to create valid llvm ir")
+                return
+            outname = exec_name
 
         try:
-            lines = subprocess.check_output("./" + TEMP_NAME,
-                                            timeout=timeout_remain,
+            lines = subprocess.check_output(outname, timeout=timeout_remain,
                                             universal_newlines=True)
         except subprocess.TimeoutExpired:
             if is_noreturn:
-                success(src_name, verbose)
+                success(src_path, verbose)
             else:
-                fail(src_name, "Executable timed out")
-            continue
+                fail(src_path, "Executable timed out")
+            return
 
         except subprocess.CalledProcessError:
             if is_except:
-                success(src_name, verbose)
+                success(src_path, verbose)
             else:
-                fail(src_name, "Unexpected exception")
-            continue
+                fail(src_path, "Unexpected exception")
+            return
 
         if is_except:
-            fail(src_name, "Expected exception")
-            continue
+            fail(src_path, "Expected exception")
+            return
 
         # Return value is last line of output
         lines = lines.split("\n")
@@ -164,16 +213,20 @@ def main():
 
         if is_return:
             if return_val != retval:
-                fail(src_name, "Returned %d expected %d" % (retval, return_val))
+                fail(src_path, "Returned %d expected %d" % (retval, return_val))
             else:
-                success(src_name, verbose)
-            continue
+                success(src_path, verbose)
+            return
 
         # Shouldn't get here
         assert(False)
 
-    if failure:
-        sys.exit(1)
+    finally:
+        for filename in temp_files:
+            try:
+                os.remove(filename)
+            except OSError:
+                pass
 
 # Run main
 main()
