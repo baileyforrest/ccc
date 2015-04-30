141ba3dae59aac125d3dcf9c923dae674e10b62e
Bailey Forrest <baileycforrest@gmail.com>
2015-04-30 02:01:10 -0400

    Added script to test current progress

---
+++ test/scripts/test_all.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TEST_15411=./test/15411
+SCRIPT_DIR=./test/scripts
+CC="./bin/ccc -S -emit-llvm"
+JOBS=16
+
+if [ -d "$TEST_15411" ]; then
+    $SCRIPT_DIR/test_runner.py -llvm "-j$JOBS" "$CC" $TEST_15411/*/*.c
+fi
+
+$SCRIPT_DIR/test_parse.bash
