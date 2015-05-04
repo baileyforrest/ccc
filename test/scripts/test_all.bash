374181d4858d567a73943293b66efc4a876e59b9
Bailey Forrest <baileycforrest@gmail.com>
2015-05-03 23:03:47 -0400

    Consolidated my tests, added them to test_all script

--- test/scripts/test_all.bash
+++ test/scripts/test_all.bash
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-TEST_15411=./test/15411
-RUNTIME_15411=./test/runtime/15411
-SCRIPT_DIR=./test/scripts
+TEST_15411=test/15411
+RUNTIME_15411=test/runtime/15411
+
+TESTS=test/tests
+RUNTIME=test/runtime
+
+SCRIPT_DIR=test/scripts
 JOBS=16
 
-CC="./bin/ccc -S -emit-llvm"
+CC="./bin/ccc"
 
+# Run 15411 Tests if present
 if [ -d "$TEST_15411" ]; then
-    $SCRIPT_DIR/test_runner.py -r $RUNTIME_15411 --llvm -j$JOBS "$CC" $TEST_15411/*/*.c
+    $SCRIPT_DIR/test_runner.py -r $RUNTIME_15411 --llvm -j$JOBS "$CC -S -emit-llvm" $TEST_15411/*/*.c
 fi
+
+# Run tests
+$SCRIPT_DIR/test_runner.py -r $RUNTIME -j$JOBS "$CC" $TESTS/*/*.c
 
 $SCRIPT_DIR/test_parse.bash
